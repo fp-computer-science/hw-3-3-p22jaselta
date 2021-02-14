a85b1260f1775e9c17ae0e7b0dfed58edc9c7381
James <p22jaselta@fairfieldprep.org>
2021-02-14 18:04:59 -0500

    Cycle 3 Day 4 Hw 3-3

---
+++ hw3-3-1.js
@@ -0,0 +1,14 @@
+// Author: CAM (AMDG) 2/14/2021
+
+var lockPick = 30
+var luck = 70 
+
+if (lockPick >= 30){
+    console.log("You Unlocked the door");
+}else if (luck >= 70){
+    console.log("You Unlocked the door");
+}else if (lockPick < 30){
+    console.log("The door is still locked");
+}else if (luck < 70){
+    console.log("The door is still locked");
+}
---
+++ hw3-3-2.js
@@ -0,0 +1,15 @@
+// Author: CAM (AMDG) 2/14/2021
+
+var levels = 50;
+
+if (levels >= 1-9) {
+    console.log("You are a beginner level");
+} else if (levels >= 10-19 ){
+    console.log("You are a low level");
+}else if(levels >= 20-29 ) {
+    console.log("You are medium level");
+}else if(levels >= 30-39){
+    console.log("You are high level");
+}else if(levels >= 40-50){
+    console.log("You are expert level");
+}
