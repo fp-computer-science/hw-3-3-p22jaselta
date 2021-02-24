d05a2b84ec7fadd3ce6345b2a669dc4b065dd18a
p22jaselta <77790721+p22jaselta@users.noreply.github.com>
2021-02-24 09:12:50 -0500

    Update hw3-3-2.js

--- hw3-3-2.js
+++ hw3-3-2.js
@@ -1,15 +1,19 @@
 // Author: CAM (AMDG) 2/14/2021
 
-var levels = 50;
+var playerLevels = 50;
 
-if (levels >= 1-9) {
+if (playerLevels >= 1 && playerLevel <= 9) {
     console.log("You are a beginner level");
-} else if (levels >= 10-19 ){
+} else if (playerLevels >= 10 && playerLevels <= 19 ){
     console.log("You are a low level");
-}else if(levels >= 20-29 ) {
+
+
+}else if(playerLevels >= 20-29 ) {
     console.log("You are medium level");
-}else if(levels >= 30-39){
+}else if(playerLevels >= 30-39){
     console.log("You are high level");
-}else if(levels >= 40-50){
+}else if(playerLevels >= 40-50){
     console.log("You are expert level");
+} else {
+    console.log("You don't appear in our records.")
 }
